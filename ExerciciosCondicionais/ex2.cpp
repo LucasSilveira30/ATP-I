db527394d34fa6c89423575fd864243f096664c9
LucasSilveira30 <silveira30082006@gmail.com>
2026-02-05 03:54:59 -0300

    Create ex2.cpp

---
+++ ExerciciosCondicionais/ex2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y, z;
+    cin >> x >> y >> z;
+
+    if (x + y > z && x + z > y && y + z > x) {
+        if (x == y && y == z) {
+            cout << "Triângulo Equilátero";
+        } else if (x == y || x == z || y == z) {
+            cout << "Triângulo Isósceles";
+        } else {
+            cout << "Triângulo Escaleno";
+        }
+    } else {
+        cout << "Essas medidas não formam um triângulo";
+    }
+
+    return 0;
+}
